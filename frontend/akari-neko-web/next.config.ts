6d7b4adc09f650baeb6ad7d751918229de507be4
Bi Nhim <BiNhim@gmail.com>
2026-05-07 21:31:06 +0700

    Delete dev test Safari

--- frontend/akari-neko-web/next.config.ts
+++ frontend/akari-neko-web/next.config.ts
@@ -1,29 +1,7 @@
 import type { NextConfig } from "next";
 
-function normalizeAllowedDevOrigin(origin: string) {
-  const trimmedOrigin = origin.trim();
-
-  if (!trimmedOrigin) {
-    return null;
-  }
-
-  const originWithoutProtocol = trimmedOrigin.replace(/^https?:\/\//, "");
-  const host = originWithoutProtocol.split(":")[0];
-
-  return host || null;
-}
-
-const allowedDevOrigins = process.env.ALLOWED_DEV_ORIGINS
-  ? process.env.ALLOWED_DEV_ORIGINS.split(",").flatMap((origin) => {
-      const normalizedOrigin = normalizeAllowedDevOrigin(origin);
-
-      return normalizedOrigin ? [normalizedOrigin] : [];
-    })
-  : [];
-
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins,
 };
 
 export default nextConfig;
